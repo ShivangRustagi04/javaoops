b124d2a409a37355e3858ee9b2b311796f5246d4
ShivangRustagi04 <113136882+ShivangRustagi04@users.noreply.github.com>
2024-01-24 08:04:00 +0000

    "learnt about object and classes"

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import java.util.Arrays;
+
 class Main {
   public static void main(String[] args){
     System.out.println("Hello world!");
+
+
+  
+    int[] rno = new int[5];
+    String[] Name = new String[5];
+    float[] marks = new float[5];
+
+    Student[] students = new Student[5];
+
+    Student kunal = new Student();
+
+    System.out.println(kunal.rno);
+    
+    System.out.println(kunal.Name);
+    System.out.println(kunal.marks);
+
+
+
   }
+    
+}
+
+
+class Student{
+    int[] rno = new int[5];
+    String[] Name = new String[5];
+    float[] marks = new float[5];
 }
